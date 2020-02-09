1f6b8f9e482afb140b8886dea091fa00f1ede18e
Dundies Online <dundies.online@gmail.com>
2020-02-09 22:28:45 +0800

    Add stomp.

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -3,9 +3,18 @@ extends Actor
 func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -speed.x;
+
+func _on_StompDetector_body_entered(body: Node) -> void:
+#	print(str(body.global_position.y) + " - " + str(get_node("StompDetector").global_position.y))
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return;
+	get_node("CollisionShape2D").disabled = true;
+	queue_free();
+	pass # Replace with function body.
 	
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta;
 	if is_on_wall():
 		_velocity.x *= -1.0;
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y;
+
